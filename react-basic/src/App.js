256083e5f11064ae6c28c2842da3cfbf044fb026
kxc5168 <kxc5168@sina.com>
2022-06-19 11:51:01 +0800

    初始化项目文件修改

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,21 +1,3 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App () {
+  return <div>根组件</div>
 }
-
-export default App;
--- react-basic/src/index.js
+++ react-basic/src/index.js
@@ -1,16 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react"
+import ReactDom from "react-dom"
+import App from "./App"
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.render(
+ReactDom.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
